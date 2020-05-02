0479be462969776da887cbf1818e0eee3e58063a
javiermixture17 <javiermf.98@gmail.com>
2020-05-02 19:13:04 +0200

    add: initial dashboard view

--- zarahealth/src/ZaraHealth.jsx
+++ zarahealth/src/ZaraHealth.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
-
 import { makeStyles, MuiThemeProvider } from "@material-ui/core/styles";
 import { Box } from "@material-ui/core";
-
 import history from "./core/misc/history";
-
 import ZaraHealthAppBar from "./ui/components/ZaraHealthAppBar.jsx";
 import ZaraHealthDrawer from "./ui/components/ZaraHealthDrawer.jsx";
-import zaraHealthTheme from "./ui/common/ZaraHealthTheme.js";
+import zaraHealthTheme from "./ui/theme/index";
 import DashboardView from "./ui/views/DashboardView";
 import FeedView from "./ui/views/FeedView";
 import SettingsView from "./ui/views/SettingsView";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import StationsDataView from "./ui/views/StationsDataView";
+import CommentsView from "./ui/views/CommentsView";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
+    minHeight: "100vh",
+    position: "relative",
+  },
+  root2: {
+    position: "relative",
+    width: "100%",
   },
 }));
 
@@ -32,18 +38,22 @@ function ZaraHealth() {
   return (
     <Router history={history}>
       <MuiThemeProvider theme={zaraHealthTheme}>
-        <ZaraHealthAppBar handleDrawerToggle={handleDrawerToggle} />
+        <CssBaseline />
         <div className={classes.root}>
           <ZaraHealthDrawer
             mobileOpen={mobileOpen}
             handleDrawerToggle={handleDrawerToggle}
           />
-          <main>
-            <Box m={1}>
+          <main className={classes.root2}>
+            <ZaraHealthAppBar handleDrawerToggle={handleDrawerToggle} />
+            <Box m={0}>
               <Switch>
                 <Route exact path="/" component={DashboardView} />
                 <Route path="/feed" component={FeedView} />
                 <Route path="/settings" component={SettingsView} />
+                <Route path="/pollen" component={StationsDataView} />
+                <Route path="/water" component={StationsDataView} />
+                <Route path="/air" component={StationsDataView} />
                 <Redirect from="*" to="/" />
               </Switch>
             </Box>
--- zarahealth/src/ui/views/DashboardView.jsx
+++ zarahealth/src/ui/views/DashboardView.jsx
@@ -1,14 +1,37 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+
+import {makeStyles} from "@material-ui/core/styles";
+import DataSummary from "../components/Dashboard/DataSummary";
+import Alerts from "../components/Dashboard/Alerts";
+import {Box, Typography} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        width: "100%",
+    },
+}));
 
 function DashboardView() {
-  return (
-    <div>
-      <Typography variant="h6">
-        DashboardView
-      </Typography>
-    </div>
-  );
+    const classes = useStyles();
+    return (
+        <Box m={1}>
+        <div className={classes.root}>
+            <Typography color="white" style={{paddingTop: 0, paddingLeft:0}}>
+                <Box fontWeight="fontWeightMedium" m={1}  fontSize={45} color="white">
+                    Hoy
+                </Box>
+            </Typography>
+            <DataSummary/>
+            <Typography color="white" style={{ paddingTop: 20, paddingLeft:0}}>
+                <Box fontWeight="fontWeightMedium" m={1}  fontSize={27} color="white">
+                    Alertas
+                </Box>
+            </Typography>
+            <Alerts/>
+        </div>
+        </Box>
+    );
 }
 
 export default DashboardView;
