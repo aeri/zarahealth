f0f7e41e942f9c9f0d4553a947834d183eb2c47c
victorpowah <g3victorrodriguez@gmail.com>
2020-05-19 03:27:30 +0200

    Test admin y settings acabados, modificacion del package.json para correr ambos archivos

--- BackEnd/GraphQL/Admin.js
+++ BackEnd/GraphQL/Admin.js
@@ -10,156 +10,158 @@ var ActivityModel = require('../mongo/model/activity.js');
 var _ = require('underscore');
 
 const {
-  GraphQLError
+    GraphQLError
 } = require('graphql')
 
 async function adminAuthentication(username) {
-  if (!username) {
-    throw new GraphQLError(`This query needs user authorization`, null, null, null, null, {
-      extensions: {
-        code: "UNAUTHORIZED",
-      }
-    })
-  } else {
-    user = await UserModel.findOne({
-      username: username
+    if (!username) {
+        throw new GraphQLError(`This query needs user authorization`, null, null, null, null, {
+            extensions: {
+                code: "UNAUTHORIZED",
+            }
+        })
+    } else {
+        user = await UserModel.findOne({
+            username: username
+        });
+
+        if (!user) {
+            throw new GraphQLError(`User ${username} not found`, null, null, null, null, {
+                extensions: {
+                    code: "INTERNAL_ERROR"
+                }
+            });
+        } else {
+            if (!user.isAdmin) {
+                throw new GraphQLError(`User ${username} is not and admin`, null, null, null, null, {
+                    extensions: {
+                        code: "UNAUTHORIZED"
+                    }
+                });
+            }
+        }
+    }
+}
+
+var retrieveUsers = async function ({
+    page,
+    limit
+}, context) {
+
+    var usernamePetition = context.response.locals.user;
+
+    //User authentication
+    await adminAuthentication(usernamePetition);
+
+    const options = {
+        page: Math.min(100, page),
+        limit: limit,
+        collation: {
+            locale: 'en'
+        }
+    };
+
+    var ret = await UserModel.paginate({}, options, function (err, result) {
+        return result.docs;
     });
 
-    if (!user) {
-      throw new GraphQLError(`User ${username} not found`, null, null, null, null, {
-        extensions: {
-          code: "INTERNAL_ERROR"
-        }
-      });
-    } else {
-      if (!user.isAdmin) {
-        throw new GraphQLError(`User ${username} is not and admin`, null, null, null, null, {
-          extensions: {
-            code: "UNAUTHORIZED"
-          }
+    return ret;
+}
+
+var updateUserStatus = async function ({
+    username,
+    status
+}, context) {
+    var usernamePetition = context.response.locals.user;
+
+    //User authentication
+    await adminAuthentication(usernamePetition);
+    if (status == "BANNED") {
+        tokenModel.deleteMany({
+            'user.username': username
+        }, function (err) {
+            if (err) logger.error(err);
         });
-      }
     }
-  }
+
+
+    return new Promise((resolve, reject) => {
+        UserModel.findOneAndUpdate({
+            username: username
+        }, {
+                $set: {
+                    status: status
+                }
+            }, {
+                new: true
+            })
+            .then((doc) => {
+                resolve(doc);
+            })
+            .catch((err) => {
+                logger.error(err);
+                reject(new GraphQLError(`Can not update this field.`, null, null, null, null, {
+                    extensions: {
+                        code: "UPDATE_FAILED",
+                    }
+                }));
+            });
+    });
 }
 
-var retrieveUsers = async function({
-  page,
-  limit
-}, context) {
+var retrieveMetrics = async function ({ }, context) {
+    var usernamePetition = context.response.locals.user;
 
-  var usernamePetition = context.response.locals.user;
+    //User authentication
+    await adminAuthentication(usernamePetition);
 
-  //User authentication
-  await adminAuthentication(usernamePetition);
+    numUsers = await UserModel.countDocuments({}, function (err, count) {
+        return count;
+    })
 
-  const options = {
-    page: Math.min(100, page),
-    limit: limit,
-    collation: {
-      locale: 'en'
-    }
-  };
-
-  var ret = await UserModel.paginate({}, options, function(err, result) {
-    return result.docs;
-  });
-
-  return ret;
-}
-
-var updateUserStatus = async function({
-  username,
-  status
-}, context) {
-  var usernamePetition = context.response.locals.user;
-
-  //User authentication
-  await adminAuthentication(usernamePetition);
-
-  tokenModel.deleteMany({
-    'user.username': username
-  }, function(err) {
-    if (err) logger.error(err);
-  });
-
-  return new Promise((resolve, reject) => {
-    UserModel.findOneAndUpdate({
-        username: username
-      }, {
-        $set: {
-          status: status
+    var actualDate = new Date();
+    activeUsers = await TokenModel.countDocuments({
+        accessTokenExpiresAt: {
+            $gt: actualDate
         }
-      }, {
-        new: true
-      })
-      .then((doc) => {
-        resolve(doc);
-      })
-      .catch((err) => {
-        logger.error(err);
-        reject(new GraphQLError(`Can not update this field.`, null, null, null, null, {
-          extensions: {
-            code: "UPDATE_FAILED",
-          }
-        }));
-      });
-  });
-}
+    }, function (err, count) {
+        return count;
+    })
 
-var retrieveMetrics = async function({}, context) {
-  var usernamePetition = context.response.locals.user;
+    numFeeds = await FeedModel.countDocuments({}, function (err, count) {
+        return count;
+    })
 
-  //User authentication
-  await adminAuthentication(usernamePetition);
+    activities = await ActivityModel.find({}, function (err, count) {
+        return count;
+    })
 
-  numUsers = await UserModel.countDocuments({}, function(err, count) {
-    return count;
-  })
-
-  var actualDate = new Date();
-  activeUsers = await TokenModel.countDocuments({
-    accessTokenExpiresAt: {
-      $gt: actualDate
+    var metric = {
+        users: numUsers,
+        activeUsers: activeUsers,
+        feeds: numFeeds,
+        activities: []
     }
-  }, function(err, count) {
-    return count;
-  })
 
-  numFeeds = await FeedModel.countDocuments({}, function(err, count) {
-    return count;
-  })
+    var groupedData = _.groupBy(activities, f => {
+        return f.action
+    });
 
-  activities = await ActivityModel.find({}, function(err, count) {
-    return count;
-  })
-
-  var metric = {
-    users: numUsers,
-    activeUsers: activeUsers,
-    feeds: numFeeds,
-    activities: []
-  }
-
-  var groupedData = _.groupBy(activities, f => {
-    return f.action
-  });
-
-  Object.keys(groupedData).forEach(function(key) {
-    var o = {
-      type: key,
-      count: groupedData[key].length
-    }
-    metric.activities.push(o);
-  })
+    Object.keys(groupedData).forEach(function (key) {
+        var o = {
+            type: key,
+            count: groupedData[key].length
+        }
+        metric.activities.push(o);
+    })
 
 
-  return metric;
+    return metric;
 
 }
 
 module.exports = {
-  retrieveUsers: retrieveUsers,
-  updateUserStatus: updateUserStatus,
-  retrieveMetrics: retrieveMetrics
+    retrieveUsers: retrieveUsers,
+    updateUserStatus: updateUserStatus,
+    retrieveMetrics: retrieveMetrics
 };
